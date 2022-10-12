453e90f17ef2ccbc84f5c9e1c0a6d48ea930e388
Alexandra Bratu <alexandra.bratu@nxdata.ro>
2022-10-12 23:13:01 +0300

    rest api django

--- 09-django/proiect/aplicatie1/tests.py
+++ 09-django/proiect/aplicatie1/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+import unittest
 
-# Create your tests here.
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import datetime
+from selenium.webdriver.chrome.service import Service
+
+
+class AuthenticationTest(unittest.TestCase):
+
+    def setUp(self):
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('detach', True)
+
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), chrome_options=options)
+        self.driver.get('http://127.0.0.1:8004')
+        username = self.driver.find_element(by=By.NAME, value='username')
+        username.send_keys('alexandra')
+        password = self.driver.find_element(by=By.NAME, value='password')
+        password.send_keys('')
+        submit = self.driver.find_element(by=By.CLASS_NAME, value='btn-info')
+        submit.submit()
+        self.logs = open('logs.txt', mode='a')
+
+    def test_form(self):
+        if value := '' in self.driver.page_source:
+            self.logs.write(f"{value}, {datetime.datetime.now()}\n")
+        assert value
